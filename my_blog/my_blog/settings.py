f7320839853a9b455f12b4c7a98d04679ded8539
Kelviz <mysviurch@gmail.com>
2022-11-26 02:47:38 -0800

    update

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Vitechy.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vibertech.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
