e7091ad754cfd415a82c67e10ee100c1bd65098f
Kelviz <mysviurch@gmail.com>
2022-11-29 05:46:37 -0800

    update

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -235,7 +235,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 CLOUDINARY_STORAGE = {
