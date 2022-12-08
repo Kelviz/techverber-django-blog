f8c59224f19f748d72a0c432fd4bcab6da8c4893
Kelviz <mysviurch@gmail.com>
2022-12-08 03:19:07 -0800

    update-s

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -17,7 +17,7 @@ import cloudinary
 import cloudinary_storage
 #import dj_database_url
 import dotenv
-from dotenv import load_dotenv, find_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
